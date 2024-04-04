439d37f9605c010392094afc3a335c73c1de1680
kumkanillam <kumkanillam@gmail.com>
2024-04-04 16:21:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
     included: function(app) {
         this._super.included(app);
 
+        app.import('vendor/element-resize-detector.js');
         app.import('vendor/raphael.min.js');
         app.import('vendor/morris.css');
         app.import('vendor/morris.js');
